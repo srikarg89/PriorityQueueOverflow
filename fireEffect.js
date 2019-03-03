7d5e7953b9a753d7a453dbce55c6074b8cb0a4cb
Ganesh <ganesh.nanduru@gmail.com>
2019-03-03 08:19:36 -0500

    stuff

--- fireEffect.js
+++ fireEffect.js
@@ -1,107 +1,64 @@
-// http://codingtra.in
-// http://patreon.com/codingtrain
-
-// Fire Effect
-// Video: https://youtu.be/X0kjv0MozuY
-// Algorithm: https://web.archive.org/web/20160418004150/http://freespace.virgin.net/hugo.elias/models/m_fire.htm
-
-var buffer1;
-var buffer2;
-var cooling;
-var w = 600;
-var h = 400;
-
-var ystart = 0.0;
+let system;
 
 function setup() {
-  createCanvas(1200, 400);
-  buffer1 = createGraphics(w, h);
-  buffer2 = createGraphics(w, h);
-  cooling = createImage(w, h, RGB);
-}
-
-function cool() {
-  cooling.loadPixels();
-  let xoff = 0.0; // Start xoff at 0
-  let increment = 0.02;
-  // For every x,y coordinate in a 2D space, calculate a noise value and produce a brightness value
-  for (let x = 0; x < w; x++) {
-    xoff += increment;   // Increment xoff
-    let yoff = ystart;   // For every xoff, start yoff at 0
-    for (let y = 0; y < h; y++) {
-      yoff += increment; // Increment yoff
-
-      // Calculate noise and scale by 255
-      let n = noise(xoff, yoff);
-      let bright = pow(n, 3) * 255;
-
-      // Try using this line instead
-      //float bright = random(0,255);
-
-      // Set each pixel onscreen to a grayscale value
-      cooling.pixels[x+y*w] = color(bright);
-    }
-  }
-
-  cooling.updatePixels();
-  ystart += increment;
-}
-
-function fire(rows) {
-//  buffer1.beginDraw();
-  buffer1.loadPixels();
-  for (let x = 0; x < w; x++) {
-    for (let j = 0; j < rows; j++) {
-      let y = h-(j+1);
-      let index = x + y * w;
-      buffer1.pixels[index] = color(255);
-    }
-  }
-  buffer1.updatePixels();
-//  buffer1.endDraw();
+  createCanvas(720, 400, WEBGL);
+  system = new ParticleSystem(createVector(0, 0));
 }
 
 function draw() {
-  fire(2);
-  console.log('hi');
-  if (mouseIsPressed) {
-//    buffer1.beginDraw();
-    buffer1.fill(255);
-    buffer1.noStroke();
-    buffer1.ellipse(mouseX, mouseY, 100, 100);
-//    buffer1.endDraw();
-  }
-  cool();
-  background(0);
-//  buffer2.beginDraw();
-  buffer1.loadPixels();
-  buffer2.loadPixels();
-  for (let x = 1; x < w-1; x++) {
-    for (let y = 1; y < h-1; y++) {
-      let index0 = (x) + (y) * w;
-      let index1 = (x+1) + (y) * w;
-      let index2 = (x-1) + (y) * w;
-      let index3 = (x) + (y+1) * w;
-      let index4 = (x) + (y-1) * w;
-      let c1 = buffer1.pixels[index1];
-      let c2 = buffer1.pixels[index2];
-      let c3 = buffer1.pixels[index3];
-      let c4 = buffer1.pixels[index4];
-      let c5 = cooling.pixels[index0];
-      let newC = brightness(c1) + brightness(c2)+ brightness(c3) + brightness(c4);
-      newC = newC * 0.25 - brightness(c5);
+  background(51);
+  system.addParticle();
+  system.run();
+}
 
-      buffer2.pixels[index4] = color(newC);
+// A simple Particle class
+let Particle = function(position) {
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.position = position.copy();
+  this.lifespan = 255;
+};
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+};
+
+// Method to display
+Particle.prototype.display = function() {
+  stroke(200, this.lifespan);
+  strokeWeight(2);
+  fill(255,0,0,this.lifespan);
+  ellipse(this.position.x, this.position.y, 12, 12);
+};
+
+// Is the particle still useful?
+Particle.prototype.isDead = function(){
+  return this.lifespan < 0;
+};
+
+let ParticleSystem = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function() {
+  this.particles.push(new Particle(this.origin));
+};
+
+ParticleSystem.prototype.run = function() {
+  for (let i = this.particles.length-1; i >= 0; i--) {
+    let p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
     }
   }
-  buffer2.updatePixels();
-//  buffer2.endDraw();
-
-  // Swap
-  let temp = buffer1;
-  buffer1 = buffer2;
-  buffer2 = temp;
-
-  image(buffer2, 0, 0);
-  image(cooling, w, 0);
-}
+};
